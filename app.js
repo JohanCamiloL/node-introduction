d685a2475661d0813df951b762b99b3a39834e37
Camilo Lopez <johan.lopez@bluetab.net>
2020-07-29 21:31:49 -0500

    Code implemented, write log in file in progress

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 const calculator = require('./calculator');
+const fs = require('fs');
 
-console.log(calculator.sum(2, 3))
+const FILE_NAME = 'log.txt';
+
+/**
+ * Writes data on file specified.
+ * @param {String} fileName File name.
+ * @param {String} data Data to be written on file.
+ */
+const writeFile = (fileName, data) => {
+    fs.exists(fileName, (exists) => {
+        if (exists) {
+            fs.appendFile(fileName, data + '\n', (error) => {
+                if (error) console.log('Error en la escritura');
+                else console.log('Escritura realizada correctamente');
+            });
+        } else {
+            fs.writeFile(fileName, data + '\n', (error) => {
+                if (error) console.log('Error en la escritura');
+                else console.log('Escritura realizada correctamente');
+            });
+        }
+    });
+}
+
+writeFile(FILE_NAME, `7 x 4 = ${calculator.mult(7, 4)}`);
+writeFile(FILE_NAME, `5 + 3 = ${calculator.sum(5, 3)}`);
